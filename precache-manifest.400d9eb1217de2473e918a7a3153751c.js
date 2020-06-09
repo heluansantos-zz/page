ac8c066a0117b6a355a7844a96b7cdcd594265e3
heluansantos <heluan.caetano@gmail.com>
2020-06-08 22:40:09 -0300

    Updates

---
+++ precache-manifest.400d9eb1217de2473e918a7a3153751c.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "304749ed26c8079c5521eab51bce9cca",
+    "url": "/page/index.html"
+  },
+  {
+    "revision": "9011b4f3237a0d88399f",
+    "url": "/page/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "2bd39526e41bdd57b1ef",
+    "url": "/page/static/js/2.e69d780f.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/page/static/js/2.e69d780f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9011b4f3237a0d88399f",
+    "url": "/page/static/js/main.427cf443.chunk.js"
+  },
+  {
+    "revision": "128f83e243111d8d4c38",
+    "url": "/page/static/js/runtime-main.71bc3007.js"
+  }
+]);
